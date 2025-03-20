3ba028e7a2ffd839259496d00cc460505c95cd6c
Shobhit Parmar <parmarshobhit91@gmail.com>
2025-03-20 23:43:56 +0530

    created routes and schema for the notes app

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = 3000;
 
 // Connect to MongoDB
@@ -10,6 +13,26 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error(err));
+
+// Schema
+const NoteSchema = new mongoose.Schema( { text: String });
+const Note = mongoose.model("Note", NoteSchema);
+
+
+// Routes
+app.get("/notes", async(req, res) => res.json(await Note.find()));
+app.post("/notes", async(req, res) => {
+    const newNote = new Note({ text: req.body.text});
+    await newNote.save()
+        .then(() => console.log("Note added successfully"))
+        .catch((err) => console.error(err));
+    res.json(newNote);
+})
+app.delete("/notes/:id", async(req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({ message: "Note deleted successfully" });
+})
+
 
 
 app.listen(PORT, () => {
