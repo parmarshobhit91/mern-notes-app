d90ccc40f7d0b997581012c513d3d6c3cfd7c372
Shobhit Parmar <parmarshobhit91@gmail.com>
2025-03-20 23:29:37 +0530

    mongodb database configured and connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const app = express();
 const PORT = 3000;
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error(err));
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
